969037d05dad5730841c375329bfae969066ba4f
nahuel <nahuel.dev.23@gmailcom>
2023-01-22 08:27:54 -0300

    line highlight on block code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,24 @@
-import { defineConfig } from 'astro/config';
-import { remarkReadingTime } from './remark-reading-time.mjs';
+import { defineConfig } from "astro/config";
+import { remarkReadingTime } from "./remark-reading-time.mjs";
 // https://astro.build/config
 import mdx from "@astrojs/mdx";
+
+import remarkPrism from 'remark-prism';
 
 // https://astro.build/config
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx({
-    remarkPlugins: [remarkReadingTime],
-    extendDefaultPlugins: true,
-  }), preact()]
+  markdown: {
+    syntaxHighlight: false,
+    remarkPlugins: [[remarkPrism, { plugins: ["line-numbers"] }]],
+  },
+   integrations: [
+      mdx({
+        remarkPlugins: [remarkReadingTime],
+        extendDefaultPlugins: true,
+      }),
+     preact(),
+   ],
 });
